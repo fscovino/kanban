d35b9cafea030b763aaab2dc676f237d74d10da6
Francisco Scovino <franciscoscovino@gmail.com>
2021-10-22 19:09:24 -0400

    update basic boilerplate code

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
-const express = require("express");
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Mongoose } from 'mongoose';
+import cors from 'cors';
+
 
 const PORT = process.env.PORT || 3001;
 
