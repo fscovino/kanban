f54853e0aba12a273382e68d0aebed4830b21f60
Francisco Scovino <franciscoscovino@gmail.com>
2021-11-11 18:40:44 -0500

    adds buttons to form

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -6,6 +6,19 @@ import icon_dots from '../../img/icon_dots.svg';
 import icon_x from '../../img/icon_x.svg';
 
 function Form() {
+
+    const hideForm = () => {
+        console.log('You are trying to hide the form');
+    }
+
+    const deleteTask = () => {
+        console.log('You are trying to delete the task');
+    }
+
+    const saveAndClose = () => {
+        console.log('You are trying to save and close the form');
+    }
+
     return (
         <div className='form'>
             <div className="form-header">
@@ -13,7 +26,7 @@ function Form() {
                     <img src={icon_dots} alt="icon dots" />
                     <h2>TASK NAME <span>(task status)</span></h2>
                 </div>
-                <img className='btn-close' src={icon_x} alt="icon close" />
+                <img className='btn-close' src={icon_x} alt="icon close" onClick={hideForm}/>
             </div>
             <form>
                 <div className="form-control span-75">
@@ -28,6 +41,21 @@ function Form() {
                     <label htmlFor="description">Description</label><br />
                     <textarea id="description" name="description" rows="10" />  
                 </div>
+                <div className="form-control span-25">
+                    <label htmlFor="owner">Assigned To</label><br />
+                    <input type="text" id="owner" name="owner" />  
+                </div>
+                <div className="form-control span-15">
+                    <label htmlFor="status">Status</label><br />
+                    <select id="status" name="status">
+                        <option value="pending">Pending</option>
+                        <option value="coding">Coding</option>
+                        <option value="testing">Testing</option>
+                        <option value="done">Done</option>
+                    </select>
+                </div>
+                <button className='btn-delete' type="button" onClick={deleteTask}>Delete</button>
+                <button className='btn-save' type="button" onClick={saveAndClose}>Save & Close</button>
             </form>
         </div>
     )
