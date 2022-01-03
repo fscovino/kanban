c44c4b9d94fbb4618f413d156016bd64e9591f01
Francisco Scovino <franciscoscovino@gmail.com>
2022-01-02 20:00:58 -0500

    Uploads final version

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import tasksRoutes from './routes/tasks.js';
 
@@ -9,6 +10,7 @@ import tasksRoutes from './routes/tasks.js';
 // Express Connection
 const PORT = process.env.PORT || 3001;
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -16,10 +18,11 @@ app.use(cors());
 
 // Set the routes for Tasks
 app.use('/task', tasksRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello, welcome to Kanban Scovino API');
+});
 
-// Database connection
-const DB_URL = 'mongodb+srv://fscovino:MongoFS14042@kanban-db.xf8nd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`)))
   .catch((error) => console.log(error.message));
